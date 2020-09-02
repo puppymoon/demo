34504d15f2c6090884b96417350f4931a7ae5a5a
黃越 <moontea@huangyuede-MacBook-Pro.local>
2020-09-02 11:04:23 +0800

    add async task

---
+++ src/main/java/com/moon/demo/component/AsyncTask.java
@@ -0,0 +1,42 @@
+package com.moon.demo.component;
+
+import java.util.concurrent.Future;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.stereotype.Component;
+
+@Component("AsyncTask")
+public class AsyncTask {
+
+	@Async
+	public Future<String> doTaskOne() throws Exception {
+		System.out.println("開始做任務一");
+		long start = System.currentTimeMillis();
+		Thread.sleep(2000);
+		long end = System.currentTimeMillis();
+		System.out.println("完成任務一，耗時：" + (end - start) + "毫秒");
+		return new AsyncResult<>("任務一完成");
+	}
+	
+	@Async
+	public Future<String> doTaskTwo() throws Exception {
+		System.out.println("開始做任務二");
+		long start = System.currentTimeMillis();
+		Thread.sleep(5000);
+		long end = System.currentTimeMillis();
+		System.out.println("完成任務二，耗時：" + (end - start) + "毫秒");
+		return new AsyncResult<>("任務二完成");
+	}
+	
+	@Async
+	public Future<String> doTaskThree() throws Exception {
+		System.out.println("開始做任務三");
+		long start = System.currentTimeMillis();
+		Thread.sleep(1000);
+		long end = System.currentTimeMillis();
+		System.out.println("完成任務三，耗時：" + (end - start) + "毫秒");
+		return new AsyncResult<>("任務三完成");
+	}
+
+}
--- src/test/java/com/moon/demo/DemoApplicationTests.java
+++ src/test/java/com/moon/demo/DemoApplicationTests.java
@@ -1,13 +1,40 @@
 package com.moon.demo;
 
+import java.util.concurrent.Future;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.moon.demo.component.AsyncTask;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private AsyncTask asyncTask;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+
+		long start = System.currentTimeMillis();
+
+		Future<String> task1 = asyncTask.doTaskOne();
+		Future<String> task2 = asyncTask.doTaskTwo();
+		Future<String> task3 = asyncTask.doTaskThree();
+
+		while (true) {
+			if (task1.isDone() && task2.isDone() && task3.isDone()) {
+				// 三個任務都呼叫完成，退出迴圈等待
+				break;
+			}
+			Thread.sleep(1000);
+		}
+
+		long end = System.currentTimeMillis();
+
+		System.out.println("任務全部完成，總耗時：" + (end - start) + "毫秒");
+
 	}
 
 }
